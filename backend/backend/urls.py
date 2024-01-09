42a6bfbb80f656935eb03f653453632926a1ea77
unknown <neharikashrestha2003@gmail.com>
2024-01-09 21:42:06 +0545

    hi

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -26,8 +26,9 @@ urlpatterns = [
     path('users/',include('users.urls')),
     path('auth/',include('djoser.urls')),
     path('auth/',include('djoser.urls.jwt')),
-  
+    #path('api/', include('events.urls')),
 ]
+  
 
 
 
